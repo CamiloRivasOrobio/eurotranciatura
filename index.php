55da9baebd3cf95c4ee74d54977c15e69ab3188d
CamiloRivasOrobio <camilorivasorobio@gmail.com>
2022-01-05 18:44:37 -0500

    proyecto euro

---
+++ index.php
@@ -0,0 +1,4 @@
+<a href="detect-labels.php">Detect Labels</a> <br>
+<a href="compare-faces.php">Compare Faces</a> <br>
+<a href="recognize-celebrities.php">ecognize Celebrities</a>
+
---
+++ js/general.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+    $(".menu").click(function () {
+        $(".keep").toggleClass("wi");
+        $(".pr").toggleClass("wi");
+        $(".main").toggleClass("wi");
+        $(".footer").toggleClass("wi");
+    });
+    $("#modificarTiempos").click(function () {
+        $("#paros").toggleClass("d-none");
+        $("#modificar").toggleClass("d-none");
+    });
+});
+function getDay(day) {
+    var d = new Date();
+    var dia = new Array(7);
+    dia[0] = "Domingo";
+    dia[1] = "Lunes";
+    dia[2] = "Martes";
+    dia[3] = "Miercoles";
+    dia[4] = "Jueves";
+    dia[5] = "Viernes";
+    dia[6] = "Sabado";
+
+    return dia[day];
+}
+
+function dateHour() {
+    var d = new Date();
+}
+
+function importData() {
+    let input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = _ => {
+        let files = Array.from(input.files);
+        let inputFile = document.getElementById("nameArchivoFile").innerHTML = files[0].name;
+        console.log(files[0].name);
+    };
+    input.click();
+
+}
+
+window.onload = function () {
+    cargarDatos();
+}
+
+function cargarDatos() {
+    var directorioActivo = [
+        { 'Id': '101', 'Name': 'Antonio Enrique Alvarado' },
+        { 'Id': '103', 'Name': 'Enrique David Ardila-' },
+        { 'Id': '104', 'Name': 'David Jose Anzola' },
+    ];
+    directorioActivo.sort();
+    addOptions("dirActivo", directorioActivo)
+}
+function addOptions(domElement, array) {
+    var select = document.getElementsByName(domElement)[0];
+    for (value in array) {
+        var option = document.createElement("option");
+        option.value = array[value].Value;
+        option.text = array[value].Name;
+        select.add(option);
+    }
+}
+function fnTiemposInactividad() {
+    $("#paros").toggleClass("d-none");
+    $("#modificar").toggleClass("d-none");
+}
